5bd79a3cf9f4dfecd3a1fc9e992473950f27d550
Samuel Joly <samuel@dedi-agency.com>
2022-12-07 15:53:39 +0100

    Getting rusty

--- autodaft/autodaft_git.zsh
+++ autodaft/autodaft_git.zsh
@@ -10,27 +10,19 @@ main() {
     zstyle ':vcs_info:git:*' formats '%F{white}%r%f%F{red}@%f%b(%F{cyan}%S%f)'
     add-zsh-hook precmd prompt_precmd
 
-#    RPROMPT='%B%F{red}λ%f%b'
+    RPROMPT='%B%F{red}λ%f%b'
     PROMPT=$'$CURRENT_PATH %B%F{yellow}>%f%b '
 }
 
 prompt_precmd() {
-    start=`date +%s%N`
-    GITSTAT=$(git status 2>/dev/null)
+#    start=`date +%s%N`
     CURRENT_PATH="%B%F{cyan}%~$f$b"
     if [[ ! -z "$vcs_info_msg_0_" ]]; then
         CURRENT_PATH="%B%F{cyan}$vcs_info_msg_0_%f%b"
-        print -P '$(prompt_git_state)'
+        STATUS=`./rush`
+        print -P "$STATUS"
     fi
-    end=`date +%s%N`
-    echo Execution time was 0.000`expr $end - $start` second
+#    end=`date +%s%N`
+#    echo Execution time was 0.000`expr $end - $start` second
 }
-
-prompt_git_state() {
-    STATUS=`~/Code/rush/target/debug/rush`
-    if [[ ! -z "$STATUS" ]]; then
-        echo "$STATUS"
-    fi
-}
-
 main
