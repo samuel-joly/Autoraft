86f90996958358d5028239683d666ba0bd4c58c1
samueljoly <samueljoly0@gmail.com>
2023-06-05 14:34:30 +0200

    Changed colorscheme

--- autodaft/autodaft_git.zsh
+++ autodaft/autodaft_git.zsh
@@ -5,20 +5,24 @@ main() {
     precmd_vcs_info() { vcs_info }
     precmd_functions+=( precmd_vcs_info )
     setopt prompt_subst
-    zstyle ':vcs_info:git:*' formats '%F{white}%r%f%F{red}@%f%b(%F{cyan}%S%f)'
+    zstyle ':vcs_info:git:*' formats '%F{magenta}%r%f%F{yellow}@%f%b(%F{cyan}%S%f)'
     add-zsh-hook precmd prompt_precmd
 
 #    RPROMPT='%B%F{red}λ%f%b'
-    PROMPT=$'$CURRENT_PATH %B%F{yellow}>%f%b '
+    PROMPT=$'%B%F{yellow}>%f%b'
 }
 
 prompt_precmd() {
 #    start=`date +%s%N`
-    CURRENT_PATH="%B%F{cyan}%~$f$b"
+    CURRENT_PATH="%B%F{magenta}%4~%f%b"
     if [[ ! -z "$vcs_info_msg_0_" ]]; then
         CURRENT_PATH="%B%F{cyan}$vcs_info_msg_0_%f%b"
-        STATUS=`rush`
-        print -P "$STATUS"
+        STATUS=`~/scripts/rush`
+        print -P "$CURRENT_PATH"
+        RPROMPT='$STATUS'
+    else
+        RPROMPT=''
+        print -P "%B%F{blue}%8d%f%b"
     fi
 #    end=`date +%s%N`
 #    echo Execution time was 0.000`expr $end - $start` second
